3131a86528b9ffbfcde61a7a2b04a9b4359cb614
varod <varodg2000@mail.ru>
2026-04-19 23:34:45 +0300

    Add log to GetPolicyDetailById func

--- internal/domain/app/policy/policy.go
+++ internal/domain/app/policy/policy.go
@@ -2,6 +2,7 @@ package policy
 
 import (
 	"context"
+	"log"
 	"reverseproxy/internal/domain/action"
 	"reverseproxy/internal/domain/policy"
 	"reverseproxy/internal/domain/rule"
@@ -85,6 +86,8 @@ func (s *AppPolicyService) GetPolicyDetailById(ctx context.Context, id primitive
 	detail.ID = id
 	detail.Name = p.Name
 	detail.WL = p.WL
+	detail.Rules = make([]policy.PolicyRuleDetail, 0, len(rules))
+	log.Printf("found %d rules\n", len(rules))
 
 	for _, rr := range p.Rules {
 		r := rulesMap[rr.RuleID]
