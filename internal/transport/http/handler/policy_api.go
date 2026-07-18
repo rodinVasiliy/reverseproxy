b4680f2c4ad7fd86ac0aeeddb6c4181d75a530ec
varod <varodg2000@mail.ru>
2026-07-18 17:14:46 +0300

    fix type of create policy request

--- internal/transport/http/handler/policy_api.go
+++ internal/transport/http/handler/policy_api.go
@@ -22,7 +22,7 @@ func RegisterPolicyRoutes(r *gin.RouterGroup, s *policy.Service, aps *appPolicy.
 		g.GET("/:id", getPolicy(aps))
 		g.DELETE("/:id", deletePolicy(s, aps))
 		g.PUT("/:id", updatePolicy(s, aps))
-		g.PUT("", createPolicy(aps))
+		g.POST("", createPolicy(aps))
 	}
 }
 
